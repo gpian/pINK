add648001230aeda0f5f0170768477f1f05410c8
Gene Pian <gene.pian@gmail.com>
2013-03-16 23:52:58 -0700

    - making a class
    potentially has bug

--- src/listener.py
+++ src/listener.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import requests
+import datetime as dt
 import os
+import requests
 import sys
 import time
 import threading
@@ -12,55 +13,76 @@ from instagram import client, subscriptions
 from SECRET import CONFIG
 from pINK import Paths
 
-counter = 0
-timer = None
+class Listener:
+    def __init__(self, api, hashtag):
+        self.api = api
+        self.hashtag = hashtag
+        self.counter = 0
+        self.paths = Paths(os.getcwd())
+        self.last_media_id = '0'
 
-paths = Paths(os.getcwd())
+    def start(self):
+        self.started_time = dt.datetime.utcnow()
+        self.grab_media()
 
-def grab_media(api, tag):
-    global counter
+    def stop(self):
+        self.timer.cancel()
+        self.timer.join()
 
-    generator = api.tag_recent_media(10, '0', tag, as_generator=True)
-    for page, next_url in generator:
-        for media in page:
-            counter = counter + 1
-            url = media.get_standard_resolution_url()
-            print "[%d] %s" % (counter, url)
+    def grab_media(self):
+        #generator = self.api.tag_recent_media(10, self.last_media_id, self.hashtag, as_generator=True)
+        generator = self.api.tag_recent_media(10, '0', self.hashtag, as_generator=True)
+        for page, next_url in generator:
+            for media in page:
 
-            filename = ''.join([paths.downloads, '/', url.split('/')[-1]])
-            r = requests.get(url)
-            if not os.path.isfile(filename):
-                with open(filename, "wb") as image:
-                    image.write(r.content)
+                url = media.get_standard_resolution_url()
+                filename = '/'.join([self.paths.downloads, url.split('/')[-1]])
 
-            url = media.user.profile_picture
-            filename = ''.join([paths.pictures, '/', url.split('/')[-1]])
-            r = requests.get(url)
-            if not os.path.isfile(filename):
-                with open(filename, "wb") as image:
-                    image.write(r.content)
+                if True:#media.created_time > self.started_time:
+                    self.counter = self.counter + 1
 
+                    print "[+] %s %s" % (media.created_time, media.id)
 
-    global timer
-    timer = threading.Timer(30, grab_media, [api, tag])
-    timer.start()
+                    # r = requests.get(url)
+                    # if not os.path.isfile(filename):
+                    #     with open(filename, "wb") as image:
+                    #         image.write(r.content)
+
+                    # url = media.user.profile_picture
+                    # filename = ''.join([paths.pictures, '/', url.split('/')[-1]])
+                    # r = requests.get(url)
+                    # if not os.path.isfile(filename):
+                    #     with open(filename, "wb") as image:
+                    #         image.write(r.content)
+
+                    try:
+                        if self.last_media_ts < media.created_time:
+                            self.last_media_ts = media.created_time
+                            self.last_media_id = media.id
+                    except AttributeError:
+                        self.last_media_ts = media.created_time
+                        self.last_media_id = media.id
+
+                else:
+                    print "[-] %s %s" % (media.created_time, media.id)
+
+        self.timer = threading.Timer(30, self.grab_media)
+        self.timer.start()
 
 def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('hashtag', help='Instagram hashtag')
     args = parser.parse_args()
 
-    unauthenticated_api = client.InstagramAPI(**CONFIG)
-    grab_media(unauthenticated_api, args.hashtag)
+    listener = Listener(client.InstagramAPI(**CONFIG), args.hashtag)
+    listener.start()
 
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
-        if timer is not None:
-            timer.cancel()
-    if timer is not None:
-        timer.join()
+        listener.stop()
+
     return 0
 
 if __name__ == '__main__':
